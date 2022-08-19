1e484e440e7a42122fc4c8a68b3e8a6868e6c4f9
gabm.remes <jin_thecraziest@live.com>
2022-08-19 16:48:49 -0300

    optimizacion know progress bar

--- src/app/layout/knowledges/knowledges.component.ts
+++ src/app/layout/knowledges/knowledges.component.ts
@@ -8,6 +8,54 @@ import { Component, OnInit } from '@angular/core';
 export class KnowledgesComponent implements OnInit {
 
   constructor() {
+
+    this.languages = [
+      [{nn : "Python",
+      progressBar : "progress-bar bg-success",
+      width : "90%"
+      },
+      {nn : "JavaScript",
+      progressBar : "progress-bar bg-primary",
+      width: "52%"
+      }],
+      [{nn : "HTML",
+      progressBar : "progress-bar bg-success",
+      width : "75%"
+      },
+      {nn : "CSS",
+      progressBar : "progress-bar bg-primary",
+      width: "62%"
+      }],
+      [{nn : "Bootstrap",
+      progressBar : "progress-bar bg-warning",
+      width : "45%"
+      },
+      {nn : "Ingles (B2)",
+      progressBar : "progress-bar bg-success",
+      width: "100%"
+      }],
+      [{nn : "Angular",
+      progressBar : "progress-bar bg-warning",
+      width : "35%"
+      },
+      {nn : "Java",
+      progressBar : "progress-bar bg-danger",
+      width: "10%"
+      }],
+      [{nn : "Spring Boot",
+      progressBar : "progress-bar bg-danger",
+      width : "1%"
+      },
+      {nn : "GIT",
+      progressBar : "progress-bar bg-danger",
+      width: "23%"
+      }],
+      [{nn : "GitHub",
+      progressBar : "progress-bar bg-warning",
+      width : "30%"
+      }]
+      
+    ]
     
    }
 
@@ -15,7 +63,7 @@ export class KnowledgesComponent implements OnInit {
 
   }
 
- 
+ languages: any[];
   
  
 }
